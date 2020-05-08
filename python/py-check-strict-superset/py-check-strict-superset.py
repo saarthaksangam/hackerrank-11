2cd7fc8399d9c34d6157fdfab4593764dd3d6f36
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-05-08 19:33:23 +0530

    Update py-check-strict-superset.py

--- python/py-check-strict-superset/py-check-strict-superset.py
+++ python/py-check-strict-superset/py-check-strict-superset.py
@@ -1,15 +1,4 @@
 #!/usr/bin/env python
-
-A = set(map(int, raw_input().split()))
-N = int(raw_input())
-
-isstrictsuperset = True
-
-for i in range(N):
-    s = set(map(int, raw_input().split()))
-    if not s.issubset(A):
-        isstrictsuperset = False
-    if len(s) >= len(A):
-        isstrictsuperset = False
-
-print isstrictsuperset
+#using generator
+a = set(input().split())
+print(all(a > set(input().split()) for _ in range(int(input()))))
